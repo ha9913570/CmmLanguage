bdbc00d33db543d0abe0dfc4442c656191cd99b3
ha9913570 <harutoharu327@gmail.com>
2026-08-10 08:55:04 +0900

    update: トークン列の表示方法を変更

--- main.cpp
+++ main.cpp
@@ -5,13 +5,14 @@
 #include "tokenize.hpp"
 
 int main(void) {
-	std::string str = "41+223 - (44*2) = ";
+	std::string str = "i=1aiue\no4s+1+223 - (44*2) = ";
 	std::vector<std::string> tokens = tokenize(str);
 
 	std::cout << "処理前:" << str << std::endl << "処理後" << std::endl;
 	for(int i = 0; i < tokens.size(); i++) {
-		std::cout << tokens[i] << std::endl;
+		std::cout << tokens[i] << ", ";
 	}
+	std::cout << std::endl;
 
 	return 0;
 }
