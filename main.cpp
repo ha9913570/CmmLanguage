f127c6b35c6eaa87812709c7e90ef48e4c782630
ha9913570 <harutoharu327@gmail.com>
2026-08-10 09:15:10 +0900

    update: 変数を表示するように変更

--- main.cpp
+++ main.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 
 #include "tokenize.hpp"
 
 int main(void) {
-	std::string str = "i=1aiue\no4s+1+223 - (44*2) = ";
+	std::string str = "a = 20\nb=19\nr=a+b\nprint(r)";
 	std::vector<std::string> tokens = tokenize(str);
+
+	std::map<std::string, std::string> vars; // 変数
+
+	for(int i = 0; i < tokens.size(); i++) {
+		if(tokens[i] == "=") {
+			std::string value = tokens[i + 1];
+			for(int j = i + 2; tokens[j] != "\n"; j++) {
+				value += tokens[j];
+			}
+			vars[tokens[i - 1]] = value;
+		}
+	}
+
+	std::cout << "変数" << std::endl;
+	for(const auto& pair : vars) {
+		std::cout << pair.first << ":" << pair.second << std::endl;
+	}
 
 	std::cout << "処理前:" << str << std::endl << "処理後" << std::endl;
 	for(int i = 0; i < tokens.size(); i++) {
