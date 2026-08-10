3946e0be89fff753e9009eef372053c85c305fe9
ha9913570 <harutoharu327@gmail.com>
2026-08-10 14:42:08 +0900

    update: 簡易的にprint関数を実装

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include "tokenize.hpp"
 
 int main(void) {
-	std::string str = "a = 20\nb=19\nr=a+b\nprint(r)";
+	std::string str = "print(\"Hello World!\")\na = 20\nb=19\nr=a+b\nprint(r)";
 	std::vector<std::string> tokens = tokenize(str);
 
 	std::map<std::string, std::string> vars; // 変数
@@ -18,15 +18,26 @@ int main(void) {
 				value += tokens[j];
 			}
 			vars[tokens[i - 1]] = value;
+		} else if(tokens[i] == "print") {
+			if(tokens[i + 2] == "\"") {
+				for(int j = i + 3; tokens[j] != "\""; j++) {
+					std::cout << tokens[j] << " ";
+				}
+				std::cout << std::endl;
+			} else {
+				std::cout << vars[tokens[i + 2]] << std::endl;
+			}
 		}
 	}
+
+	std::cout << "\n==デバッグ用==" << std::endl;
 
 	std::cout << "変数" << std::endl;
 	for(const auto& pair : vars) {
 		std::cout << pair.first << ":" << pair.second << std::endl;
 	}
 
-	std::cout << "処理前:" << str << std::endl << "処理後" << std::endl;
+	std::cout << "処理前\n" << str << std::endl << "処理後" << std::endl;
 	for(int i = 0; i < tokens.size(); i++) {
 		std::cout << tokens[i] << ", ";
 	}
