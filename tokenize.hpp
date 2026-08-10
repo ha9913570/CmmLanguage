31b29fd2ceec57a67561b3af51fe70225254e3c2
ha9913570 <harutoharu327@gmail.com>
2026-08-11 08:42:37 +0900

    update: インクルードガードを追加

--- tokenize.hpp
+++ tokenize.hpp
@@ -1,6 +1,11 @@
+#ifndef TOKENIZE
+#define TOKENIZE
+
 #include "token.hpp"
 
 bool isSymbol(char c);
 bool isNumber(char c);
 bool isSpace(char c);
 std::vector<Token> tokenize(std::string s);
+
+#endif // TOKENIZE
