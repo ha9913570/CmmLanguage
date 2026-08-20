7ee514bfc9b92b81f3034f40fa1903159d33c292
ha9913570 <harutoharu327@gmail.com>
2026-08-20 19:17:11 +0900

    refactor: Tokenクラスをファイル分割

--- include/token.hpp
+++ include/token.hpp
@@ -8,20 +8,8 @@ class Token {
 		std::string tokenType;
 		std::string token;
 
-		void createToken(std::string type, std::string value) {
-			tokenType = type;
-			token = value;
-		}
-
-		std::string toString() {
-			if(token == "\n") {
-				return tokenType + " : \\n\n"; 
-			} else if(token == "\0") {
-				return tokenType + " : \\0\n";
-			} else {
-				return tokenType + " : " + token + "\n";
-			}
-		}
+		void createToken(std::string type, std::string value);
+		std::string toString();
 };
 
 #endif // TOKEN
---
+++ src/token.cpp
@@ -0,0 +1,16 @@
+#include "token.hpp"
+
+void Token::createToken(std::string type, std::string value) {
+	tokenType = type;
+	token = value;
+}
+
+std::string Token::toString() {
+	if(token == "\n") {
+		return tokenType + " : \\n\n";
+	} else if(token == "\0") {
+		return tokenType + " : \\0\n";
+	} else {
+		return tokenType + " : " + token + "\n";
+	}
+}
