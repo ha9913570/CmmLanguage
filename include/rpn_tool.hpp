0e4992df62729102336bf12a01cf840cd9ef607e
ha9913570 <harutoharu327@gmail.com>
2026-08-24 20:14:45 +0900

    refactor: includeガードを追加

--- include/rpn_tool.hpp
+++ include/rpn_tool.hpp
@@ -1,5 +1,10 @@
+#ifndef RPN_TOOL
+#define RPN_TOOL
+
 #include <string>
 #include <vector>
 
 std::vector<std::string> InfixNotationToRPN(std::vector<std::string> infixArray);
 std::string calcRPN(std::vector<std::string> rpnArray);
+
+#endif  // RPN_TOOL
